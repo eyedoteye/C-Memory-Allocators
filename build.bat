28f2a3dfe291a8dc49c3055433cac22ca44d5283
sigmasleep <sarkar.krishnan@gmail.com>
2017-05-05 12:24:20 -0400

    Improve Build Macros

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ IF NOT EXIST build mkdir build
 pushd build
 
 set CompilerFlags=-MTd -nologo -GR- -EHsc -Oi -W4 -FC -Z7 -DWINDOWS=1
-
-cl %CompilerFlags% ../main.cpp /link Kernel32.lib
+set PassedParameters=%*
+cl %PassedParameters% %CompilerFlags% ../main.cpp /link Kernel32.lib
